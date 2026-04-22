53fb9044019c19235b64a7b1c0b63174265c501e
Zeng Xuyang <87986548+PEKKARam@users.noreply.github.com>
2026-04-22 20:31:56 +0800

    update in 4/22, hello cuda

--- hello_cuda/hello.cu
+++ hello_cuda/hello.cu
@@ -1,8 +1,9 @@
-#include <stdio.h>
+# include <stdio.h>
+# include <cuda_runtime.h>
 
 // a simple CUDA kernel function that prints "Hello World" from the GPU
 __global__ void hello_world() {
-    printf("GPU: Hello World\n");
+    printf("GPU: Hello World! I am thread %d\n", threadIdx.x);
 }
 
 int main() {
@@ -11,3 +12,7 @@ int main() {
     cudaDeviceReset(); // if no this line ,it can not output hello world from gpu
     return 0;
 }
+
+
+// compile and run
+// nvcc hello.cu -o hello | ./hello
