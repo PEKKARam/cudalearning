ade3968b180f7303e5c1f5deaa74ebf20d5acd27
Zeng Xuyang <87986548+PEKKARam@users.noreply.github.com>
2026-04-22 20:10:25 +0800

    new env

--- hello_cuda/hello.cu
+++ hello_cuda/hello.cu
@@ -1,12 +1,13 @@
-# include <stdio.h>
+#include <stdio.h>
 
+// a simple CUDA kernel function that prints "Hello World" from the GPU
 __global__ void hello_world() {
-    printf ("GPU: Hello World\n");
+    printf("GPU: Hello World\n");
 }
 
-int main(){
+int main() {
     printf("CPU: Hello CUDA\n");
-    hello_world<<<1, 1>>>();
+    hello_world<<<1, 10>>>();
     cudaDeviceReset(); // if no this line ,it can not output hello world from gpu
     return 0;
 }
